a788c487d6fe4a18d7d4619ca82cbb611e416c41
Hari Jalendran <hari.jalendran16@gmail.com>
2021-11-28 20:44:14 +0000

    added data into all array

--- randomMessages.js
+++ randomMessages.js
@@ -1,6 +1,6 @@
 //object to store the quotes, colour and cuisine
 const allData = {
-    luckyCol =['red','blue','green','yellow','pink','orange','black','white'],
-    motivationalQuotes =[],
-    cuisine = []
+    luckyCol : ['red','blue','green','yellow','pink','orange','black','white'],
+    motivationalQuotes : ["Good, better, best. Never let it rest. 'Til your good is better and your better is best.","It always seems impossible until it's done.","Life is 10% what happens to you and 90% how you react to it.","When something is important enough, you do it even if the odds are not in your favor.","Start where you are. Use what you have. Do what you can.","With the new day comes new strength and new thoughts."],
+    cuisine : ['Chinese','Indian','Italian','Greek','Arabic','Thai','Japanese','Nigerian']
 }
