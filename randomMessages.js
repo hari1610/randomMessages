dff87ffc10b82d46188784de0375cbc905a20b4a
Hari Jalendran <hari.jalendran16@gmail.com>
2021-11-28 15:49:19 +0000

    created data object and added colour array

---
+++ randomMessages.js
@@ -0,0 +1,6 @@
+//object to store the quotes, colour and cuisine
+const allData = {
+    luckyCol =['red','blue','green','yellow','pink','orange','black','white'],
+    motivationalQuotes =[],
+    cuisine = []
+}
